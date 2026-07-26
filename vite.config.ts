4c5c3c6772f2a4217ee7d9ecec7833fd03430911
Shadow <loud23for@gmail.com>
2026-07-26 23:51:28 +0800

    gengx

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [
+  base: '/Whisperbox/',
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
